feb21df5ab2944a56504e0dd905fc1f2207766b3
Mariusz <mariusz@Mariusz-ubuntu>
2013-07-24 10:28:56 +0200

    first commit, creating a project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KarolShop::Application.config.secret_token = 'ae1d0870b458fb1c14631a9f2fd65e64ff272705dbfb77773e8ec7c904c186b47b658a40c1d0b713317c6366c36546fdd8dba34e977d3a9f805a3311dfb9c5c6'
