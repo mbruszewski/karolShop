e70a11d8aeebd23a0c48364369d17d2423f88d12
Mariusz <mariusz@Mariusz-ubuntu>
2013-07-26 15:52:46 +0200

    na zakonczenie dnia

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,15 +7,14 @@ describe User do
   end
 
 	let(:user) { FactoryGirl.create(:user) }
-=begin
-	let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-=end
-	#attr_accessible :city, :street_and_number, :post_code
+
 	let!(:a1) { FactoryGirl.create(:address, user_id: user, city: "Bialystok", street_and_number: "Tab 13", post_code: "15-633") }
 
+	subject { @user }
 
 	it { should respond_to(:login) }
 	it { should respond_to(:password) }
+
 
 
 end
