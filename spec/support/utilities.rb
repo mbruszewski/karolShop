88654ec8ea84bf5954ab11286dd7eae584e6009d
Mariusz <mariusz@Mariusz-ubuntu>
2013-08-20 12:58:12 +0200

    Finished button style

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -5,6 +5,8 @@ include ApplicationHelper
 
 def sign_in(user)
 	visit login_path
+  include 'pry'
+  binding.pry
 	fill_in "Login", with: user.login
 	fill_in	"Hasło", with: user.password
 	click_button "Zaloguj"
