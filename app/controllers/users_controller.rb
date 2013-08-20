f43940af783cb24de0d5a214f604de750c7578d2
Mariusz <mariusz@Mariusz-ubuntu>
2013-08-20 14:36:31 +0200

    made avaliable to enter edit page for normal users and repaired tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     pp current_user.id
     pp params[:id].to_i
 
-		if params[:id] == current_user.id || current_user.admin
+		if params[:id].to_i == current_user.id || current_user.admin
     	@user = User.find(params[:id])
 		else
 			redirect_to root_path
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   end
 
   def update
-		if params[:id] == current_user.id || current_user.admin
+		if params[:id].to_i == current_user.id || current_user.admin
 		  @user = User.find(params[:id])
 		  if @user.update_attributes(params[:user])
 		    redirect_to root_path, :notice  => t("flash.edit", item: t("controller.user"))
