6fddd0f477da6ce2d62bbdf63f9febddbe7b63d6
Mariusz <mariusz@Mariusz-ubuntu>
2013-08-26 16:04:11 +0200

    added ordering products to page, need to fix that still

--- db/migrate/20130808083031_change_model_id_and_product_id_for_models_products.rb
+++ db/migrate/20130808083031_change_model_id_and_product_id_for_models_products.rb
@@ -1,6 +1,10 @@
 # -*- encoding : utf-8 -*-
 class ChangeModelIdAndProductIdForModelsProducts < ActiveRecord::Migration
   def up
+    change_table :models_products do |t|
+      t.change :product_id, :integer
+      t.change :model_id, :integer
+    end  
   end
 
   def down
